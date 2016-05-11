fb0ac731c417577f42f0dbf8c1ec977337579624
jono <joejond@gmail.com>
2016-05-12 06:21:41 +0800

    update ono foldere

--- posisi.php
+++ posisi.php
@@ -3,12 +3,10 @@
 include "koneksi.php";
 
 try{
-	//*
 	$time = isset($_GET['t'])?$_GET['t'] : '07:00';
 	$comp = isset($_GET['c'])?$_GET['c'] : '0';
 	$cc = substr($time,0,1);
 	$tz = ($cc == "-") ? $time :  "+".$time ; 
-	// echo $tz ."<br>"; 
 	
 	$q_wkt = "select max(data_time) wkt 
 				from data d1 
@@ -16,21 +14,15 @@ try{
 					join ship s on s.id_ship = tu1.id_ship
 				where s.status = 1
 				group by tu1.id_ship;";
-	
-	// echo $q_wkt."<br>";
 	$stm = $conn->prepare($q_wkt);
 	$stm->execute();
 	$hsl = $stm->fetchAll(PDO::FETCH_OBJ);
-	// $hsl = $stm->fetchAll(PDO::FETCH_ASSOC);
 
 	$a = "'";
 	foreach ($hsl as $s){
 		$a .= $s->wkt."','";
 	}
-	
-	// echo $a;
 	$b = substr($a,0,-2);
-	// echo $b;
 	
 	$c = "select tu.id_ship id, s.name ves, convert_tz(from_unixtime(d.epochtime),'+07:00','$tz') wkt,
 			max(case when tu.id_data_type = 1 then round(d.value,2) end) lat, 
@@ -45,56 +37,36 @@ try{
 	$stm->execute();
 	$posisi = $stm->fetchAll(PDO::FETCH_OBJ);
 
-	// $jsonResult = array(
-		// 'success' => true,
-		// 'posisi' => $posisi
-	// );
-	//*/
-	
 	//================================================================================
-	// Creates the Document.
 	$dom = new DOMDocument('1.0', 'UTF-8');
-	
-	// Creates the root KML element and appends it to the root document.
-	// $node = $dom->createElementNS('http:"//earth.google.com/kml/2.1, 'kml');
-	$node = $dom->createElementNS("http://www.opengis.net/kml/2.2", 'kml');
- 
-	$parNode = $dom->appendChild($node);
-
-	// Creates a KML Document element and append it to the KML element.
-	$dnode = $dom->createElement('Document');
-	$docNode = $parNode->appendChild($dnode);
-
+		$node = $dom->createElementNS("http://www.opengis.net/kml/2.2", 'kml');
+		$parNode = $dom->appendChild($node);
+			$dnode = $dom->createElement('Document');
+			$docNode = $parNode->appendChild($dnode);
+			
+				$Fnode = $dom->createElement('Folder');
+				$FoldNode = $docNode->appendChild($Fnode);
+					$lnode = $dom->createElement('Position');
+					$LokNode = $FoldNode->appendChild($lnode);
+			
 	foreach($posisi as $pos){
-		$node = $dom->createElement('Placemark');
-		$placeNode = $docNode->appendChild($node);
-
-		// Creates an id attribute and assign it the value of id column.
-		$placeNode->setAttribute('id', 'placemark' . $pos->id);
-
-		// Create name, and description elements and assigns them the values of the name and address columns from the results.
-		// $nameNode = $dom->createElement('name',htmlentities($row['name']));
-		$nameNode = $dom->createElement('name',htmlentities($pos->ves));
-		$placeNode->appendChild($nameNode);
-		$descNode = $dom->createElement('time', $pos->wkt);
-		$placeNode->appendChild($descNode);
-		// $styleUrl = $dom->createElement('styleUrl', '#' . $loks->type . 'Style');
-		// $placeNode->appendChild($styleUrl);
-		// Creates a Point element.
-		$pointNode = $dom->createElement('Point');
-		$placeNode->appendChild($pointNode);
-
-		// Creates a coordinates element and gives it the value of the lng and lat columns from the results.
-		$coorStr = $pos->lng . ','  . $pos->lat;
-		$coorNode = $dom->createElement('coordinates', $coorStr);
-		$pointNode->appendChild($coorNode);
-		
+		$plcnode = $dom->createElement('Placemark');
+		$lnode = $docNode->appendChild($plcnode);
+		$lnode->setAttribute('id', $pos->id);
+			$nameNode = $dom->createElement('name',htmlentities($pos->ves));
+			$lnode->appendChild($nameNode);
+			$timeNode = $dom->createElement('time', $pos->wkt);
+			$lnode->appendChild($timeNode);
+			$pNode = $dom->createElement('Point');
+			$lnode->appendChild($pNode);
+				$coorStr = $pos->lng . ','  . $pos->lat;
+				$coorNode = $dom->createElement('coordinates', $coorStr);
+				$pNode->appendChild($coorNode);
 	}
 
 	
 	$kmlOutput = $dom->saveXML();
 	header('Content-type: application/vnd.google-earth.kml+xml');
-	// header('Content-type: application/xml');
 	echo $kmlOutput;
 	
 	//================================================================================

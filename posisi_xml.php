faba1b7f315a6111454bbd37d9e053ac5bc83809
jono <jono.daunbiru@gmail.com>
2016-05-12 08:10:23 +0700

    di paksa

--- posisi_xml.php
+++ posisi_xml.php
@@ -8,12 +8,13 @@ try{
 	$cc = substr($time,0,1);
 	$tz = ($cc == "-") ? $time :  "+".$time ; 
 	
-	$q_wkt = "select max(data_time) wkt 
+	$q_wkt = "select max(convert_tz(from_unixtime(d1.epochtime),'+07:00','$tz')) wkt 
 				from data d1 
 					join titik_ukur tu1 on tu1.id_titik_ukur = d1.id_titik_ukur 
 					join ship s on s.id_ship = tu1.id_ship
 				where s.status = 1
 				group by tu1.id_ship;";
+	
 	$stm = $conn->prepare($q_wkt);
 	$stm->execute();
 	$hsl = $stm->fetchAll(PDO::FETCH_OBJ);
@@ -22,7 +23,9 @@ try{
 	foreach ($hsl as $s){
 		$a .= $s->wkt."','";
 	}
+	
 	$b = substr($a,0,-2);
+	
 	$c = "select tu.id_ship id, s.name ves, convert_tz(from_unixtime(d.epochtime),'+07:00','$tz') wkt,
 			max(case when tu.id_data_type = 1 then round(d.value,2) end) lat, 
 			max(case when tu.id_data_type = 2 then round(d.value,2) end) lng 
@@ -38,38 +41,61 @@ try{
 
 	
 	//================================================================================
-	$dom = new DOMDocument('1.0', 'UTF-8');
-		$node = $dom->createElementNS("http://www.opengis.net/kml/2.2", 'kml');
-		$parNode = $dom->appendChild($node);
-			$dnode = $dom->createElement('Document');
-			$docNode = $parNode->appendChild($dnode);
-			
-				$Fnode = $dom->createElement('Folder');
-				$FoldNode = $docNode->appendChild($Fnode);
-					$lnode = $dom->createElement('name','Position');
-					$LokNode = $FoldNode->appendChild($lnode);
-			
-	foreach($posisi as $pos){
-		$plcnode = $dom->createElement('Placemark');
-		$PlaceNode = $FoldNode->appendChild($plcnode);
-		$plcnode->setAttribute('id', $pos->id);
-			$nameNode = $dom->createElement('name',htmlentities($pos->ves));
-			$plcnode->appendChild($nameNode);
-			$timeNode = $dom->createElement('time', $pos->wkt);
-			$plcnode->appendChild($timeNode);
-			$pNode = $dom->createElement('Point');
-			$plcnode->appendChild($pNode);
-				$coorStr = $pos->lng . ','  . $pos->lat;
-				$coorNode = $dom->createElement('coordinates', $coorStr);
-				$pNode->appendChild($coorNode);
-		
-	}
-
 	
+	$dom = new DOMDocument('1.0', 'UTF-8');
+	$node = $dom->createElementNS("http://www.opengis.net/kml/2.2", 'kml');
+	$ParentNode = $dom->appendChild($node);
+
+		$documentNode = $dom->createElement('Document');
+		$doc = $ParentNode->appendChild($documentNode);
+
+		$docnameNode = $dom->createElement('name','Vessel Tracking');
+		$doc->appendChild($docnameNode);
+		
+		$docdescNode = $dom->createElement('description','Tracking for Marine Vessel');
+		$doc->appendChild($docdescNode);
+		
+		$folderNode = $dom->createElement('Folder');
+		$parFolder = $doc->appendChild($folderNode);
+			$foldnameNode = $dom->createElement('name','Position');
+			$parFolder->appendChild($foldnameNode);
+			
+			foreach($posisi as $pos){
+				$placemark = $dom->createElement('Placemark');
+				$placenode = $parFolder->appendChild($placemark);
+					$placenama = $dom->createElement('name',$pos->ves);
+					$placenode->appendChild($placenama);
+					
+					$wkt = $dom->createElement('time', $pos->wkt);
+					$placenode->appendChild($wkt);
+					
+					$point = $dom->createElement('Point');
+					$pointNode = $placenode->appendChild($point);
+					
+					$coord = $pos->lng.",".$pos->lat;
+					$koordinat = $dom->createElement('coordinates',$coord);
+					$pointNode->appendChild($koordinat);
+			}
+			
+		
+		
+		$folderNode = $dom->createElement('Folder');
+		$parFolder = $doc->appendChild($folderNode);
+			$foldnameNode = $dom->createElement('name','Tracking 24H');
+			$parFolder->appendChild($foldnameNode);
+		
+
+		$folderNode = $dom->createElement('Folder');
+		$parFolder = $doc->appendChild($folderNode);
+			$foldnameNode = $dom->createElement('name','Tracking Today');
+			$parFolder->appendChild($foldnameNode);		
+				
+
 	$kmlOutput = $dom->saveXML();
-	// header('Content-type: application/vnd.google-earth.kml+xml');
+		// header('Content-type: application/vnd.google-earth.kml+xml');
 	header('Content-type: application/xml');
 	echo $kmlOutput;
+	
 	
 	//================================================================================
 	
